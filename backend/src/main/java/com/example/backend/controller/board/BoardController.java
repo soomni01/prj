25f8956356a0ed7e3e693541d03970d5837078ff
soomni01 <dsgh8933@naver.com>
2024-11-14 16:25:02 +0900

    삭제 후 토스트 출력, 목록으로 이동

--- backend/src/main/java/com/example/backend/controller/board/BoardController.java
+++ backend/src/main/java/com/example/backend/controller/board/BoardController.java
@@ -16,14 +16,22 @@ public class BoardController {
 
     final BoardService service;
 
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Map<String, Object>> delete(@PathVariable int id) {
+        if (service.remove(id)) {
+            return ResponseEntity.ok()
+                    .body(Map.of("message", Map.of("type", "success",
+                            "text", STR."\{id}번 게시글이 삭제되었습니다.")));
+        } else {
+            return ResponseEntity.internalServerError()
+                    .body(Map.of("message", Map.of("type", "error",
+                            "text", "게시글 삭제 중 문제가 발생하였습니다.")));
+        }
+    }
+
     @GetMapping("view/{id}")
     public Board view(@PathVariable int id) {
         return service.get(id);
-    }
-
-    @DeleteMapping("delete/{id}")
-    public void delete(@PathVariable int id) {
-        service.remove(id);
     }
 
     @GetMapping("list")
