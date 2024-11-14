8130da1214b820ffd8206d75d1755ed0d252dd20
soomni01 <dsgh8933@naver.com>
2024-11-14 15:23:54 +0900

    글 작성 성공/실패 시 다른 기능 실행

--- backend/src/main/java/com/example/backend/service/board/BoardService.java
+++ backend/src/main/java/com/example/backend/service/board/BoardService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Map;
 
 @Service
 @Transactional
@@ -16,16 +15,10 @@ public class BoardService {
 
     final BoardMapper mapper;
 
-    public Map<String, Object> add(Board board) {
+    public Boolean add(Board board) {
         int cnt = mapper.insert(board);
 
-        if (cnt == 1) {
-            return Map.of("message", Map.of("type", "success",
-                            "text", STR."\{board.getId()}번 게시물이 등록되었습니다"),
-                    "data", board);
-        } else {
-            return null;
-        }
+        return cnt == 1;
     }
 
     public List<Board> list() {
--- frontend/src/page/board/BoardAdd.jsx
+++ frontend/src/page/board/BoardAdd.jsx
@@ -28,6 +28,13 @@ export function BoardAdd() {
         });
 
         navigate(`/view/${data.data.id}`);
+      })
+      .catch((e) => {
+        const message = e.response.data.message;
+        toaster.create({
+          description: message.text,
+          type: message.type,
+        });
       });
   };
 
