033669cc39e0f3babce95c09b0733b6732797760
soomni01 <dsgh8933@naver.com>
2024-11-14 10:25:49 +0900

    react 경로 설정
    / : 게시물 목록
    /add : 게시물 작성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,70 @@
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  useNavigate,
+} from "react-router-dom";
+import { Box, Flex, Stack } from "@chakra-ui/react";
+
+function Navbar() {
+  const navigate = useNavigate();
+
+  return (
+    <Flex gap={3}>
+      <Box onClick={() => navigate("/")}>HOME</Box>
+      <Box onClick={() => navigate("/add")}>작성</Box>
+    </Flex>
+  );
+}
+
+function RootLayout() {
+  return (
+    <Stack>
+      <Box>
+        <Navbar />
+      </Box>
+      <Box>
+        <Outlet />
+      </Box>
+    </Stack>
+  );
+}
+
+function BoardList() {
+  return (
+    <Box>
+      <h3>게시물 목록</h3>
+    </Box>
+  );
+}
+
+function BoardAdd() {
+  return (
+    <Box>
+      <h3>게시물 작성</h3>
+    </Box>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <BoardList />,
+      },
+      {
+        path: "add",
+        element: <BoardAdd />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <>hello my project</>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
