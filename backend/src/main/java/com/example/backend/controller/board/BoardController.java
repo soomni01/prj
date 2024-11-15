7b079f1b39985e82151b70cb894538da94f3c32f
soomni01 <dsgh8933@naver.com>
2024-11-15 12:35:27 +0900

    검색버튼 클릭시 검색조건과 키워드가 query string으로 서버에 전달됨

--- backend/src/main/java/com/example/backend/controller/board/BoardController.java
+++ backend/src/main/java/com/example/backend/controller/board/BoardController.java
@@ -54,7 +54,12 @@ public class BoardController {
     }
 
     @GetMapping("list")
-    public Map<String, Object> list(@RequestParam(value = "page", defaultValue = "1") Integer page) {
+    public Map<String, Object> list(
+            @RequestParam(value = "page", defaultValue = "1") Integer page,
+            @RequestParam(value = "st", defaultValue = "all") String searchType,
+            @RequestParam(value = "sk", defaultValue = "") String keyword) {
+        System.out.println(searchType);
+        System.out.println(keyword);
         return service.list(page);
     }
 
