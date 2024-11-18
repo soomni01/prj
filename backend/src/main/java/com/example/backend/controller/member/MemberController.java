a9e1cc88d4402527b532ed9e7b144e2b4ff28445
soomni01 <dsgh8933@naver.com>
2024-11-18 10:39:25 +0900

    회원 탈퇴

--- backend/src/main/java/com/example/backend/controller/member/MemberController.java
+++ backend/src/main/java/com/example/backend/controller/member/MemberController.java
@@ -18,8 +18,18 @@ public class MemberController {
     final MemberService service;
 
     @DeleteMapping("remove")
-    public void remove(@RequestBody Member member) {
-        service.remove(member);
+    public ResponseEntity<Map<String, Object>> remove(@RequestBody Member member) {
+        if (service.remove(member)) {
+            //잘됨
+            return ResponseEntity.ok(Map.of("message",
+                    Map.of("type", "success",
+                            "text", "회원정보를 삭제하였습니다.")));
+        } else {
+            return ResponseEntity.badRequest()
+                    .body(Map.of("message",
+                            Map.of("type", "warning",
+                                    "text", "정확한 정보를 입력해주세요.")));
+        }
     }
 
     @GetMapping("{id}")
