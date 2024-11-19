cf8940193ea8686a63cc993f43dfd15635aef229
soomni01 <dsgh8933@naver.com>
2024-11-19 14:53:33 +0900

    admin만 회원목록 조회 가능

--- backend/src/main/java/com/example/backend/controller/member/MemberController.java
+++ backend/src/main/java/com/example/backend/controller/member/MemberController.java
@@ -101,6 +101,7 @@ public class MemberController {
 
 
     @GetMapping("list")
+    @PreAuthorize("hasAuthority('SCOPE_admin')")
     public List<Member> list() {
         return service.list();
     }
