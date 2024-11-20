326d96cf8c70d6570fcb2d6607d642792b2e8337
soomni01 <dsgh8933@naver.com>
2024-11-20 15:12:41 +0900

    댓글 작성자만 수정 가능하게

--- backend/src/main/java/com/example/backend/controller/comment/CommentController.java
+++ backend/src/main/java/com/example/backend/controller/comment/CommentController.java
@@ -20,13 +20,17 @@ public class CommentController {
 
     @PutMapping("edit")
     @PreAuthorize("isAuthenticated()")
-    public ResponseEntity<Map<String, Object>> edit(@RequestBody Comment comment) {
-        if (service.update(comment)) {
-            return ResponseEntity.ok().body(Map.of("message", Map.of("type", "success",
-                    "text", "댓글이 수정되었습니다.")));
+    public ResponseEntity<Map<String, Object>> edit(@RequestBody Comment comment, Authentication authentication) {
+        if (service.hasAccess(comment.getId(), authentication)) {
+            if (service.update(comment)) {
+                return ResponseEntity.ok().body(Map.of("message", Map.of("type", "success",
+                        "text", "댓글이 수정되었습니다.")));
+            } else {
+                return ResponseEntity.internalServerError().body(Map.of("message", Map.of("type", "error",
+                        "text", "댓글이 수정되지 않았습니다.")));
+            }
         } else {
-            return ResponseEntity.internalServerError().body(Map.of("message", Map.of("type", "error",
-                    "text", "댓글이 수정되지 않았습니다.")));
+            return ResponseEntity.status(403).build();
         }
     }
 
