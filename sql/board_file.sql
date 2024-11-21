42a7c2d8e30ff441f433e3b8a846be63244098b5
soomni01 <dsgh8933@naver.com>
2024-11-21 10:43:11 +0900

    게시물 목록 조회시 파일 수 계산 쿼리

--- sql/board_file.sql
+++ sql/board_file.sql
@@ -10,3 +10,23 @@ CREATE TABLE board_file
 
 SELECT *
 FROM board_file;
+
+# 게시물 목록 조회 (w/ 파일수)
+# 방법 1 (조인)
+SELECT b.id, b.title, COUNT(DISTINCT c.id) 댓글수, COUNT(DISTINCT f.name) 파일수
+FROM board b
+         LEFT JOIN comment c
+                   ON b.id = c.board_id
+         LEFT JOIN board_file f
+                   ON b.id = f.board_id
+GROUP BY b.id
+ORDER BY b.id DESC;
+
+# 방법 2 (서브쿼리)
+SELECT b.id,
+       b.title,
+       (SELECT COUNT(*) FROM comment c WHERE c.board_id = b.id)    댓글수,
+       (SELECT COUNT(*) FROM board_file f WHERE f.board_id = b.id) 파일수
+FROM board b
+GROUP BY b.id
+ORDER BY b.id DESC;
