dec7e5771fcb345ac77693daf298229b5b723955
soomni01 <dsgh8933@naver.com>
2024-11-21 15:32:16 +0900

    테이블 생성 쿼리 수정

--- sql/board.sql
+++ sql/board.sql
@@ -5,7 +5,7 @@ CREATE TABLE board
     id       INT PRIMARY KEY AUTO_INCREMENT,
     title    VARCHAR(300)  NOT NULL,
     content  VARCHAR(5000) NOT NULL,
-    writer   VARCHAR(100)  NOT NULL,
+    writer   VARCHAR(20)   NOT NULL REFERENCES member (id),
     inserted DATETIME DEFAULT NOW()
 );
 
--- sql/member.sql
+++ sql/member.sql
@@ -3,6 +3,7 @@ USE prj20241114;
 CREATE TABLE member
 (
     id          VARCHAR(20) PRIMARY KEY,
+    email       VARCHAR(300) UNIQUE,
     password    VARCHAR(30) NOT NULL,
     description VARCHAR(1000),
     inserted    DATETIME DEFAULT NOW()
