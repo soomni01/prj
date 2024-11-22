ba521cc388b7d1b209dd61faa401b806cdc256a6
soomni01 <dsgh8933@naver.com>
2024-11-22 17:01:06 +0900

    댓글 css

--- frontend/src/components/comment/CommentInput.jsx
+++ frontend/src/components/comment/CommentInput.jsx
@@ -2,6 +2,7 @@ import { Box, Group, Textarea } from "@chakra-ui/react";
 import { Button } from "../ui/button.jsx";
 import { useContext, useState } from "react";
 import { AuthenticationContext } from "../context/AuthenticationProvider.jsx";
+import { FaPaperPlane } from "react-icons/fa";
 
 export function CommentInput({ boardId, onSaveClick }) {
   const [comment, setComment] = useState("");
@@ -9,21 +10,31 @@ export function CommentInput({ boardId, onSaveClick }) {
 
   return (
     <Box>
-      <Group>
+      <Group h={125} attached w={"100%"}>
         <Textarea
+          h={127}
+          flex={1}
           value={comment}
           disabled={!isAuthenticated}
-          placeholder={isAuthenticated ? "" : "로그인 후 댓글을 남겨주세요."}
+          placeholder={
+            isAuthenticated
+              ? "댓글을 남겨주세요."
+              : "로그인 후 댓글을 남겨주세요."
+          }
           onChange={(e) => setComment(e.target.value)}
         />
         <Button
+          h={125}
           disabled={!isAuthenticated}
           onClick={() => {
             setComment("");
             onSaveClick(comment);
           }}
+          variant={"surface"}
+          colorPalette={"blue"}
+          px={5}
         >
-          댓글 쓰기
+          <FaPaperPlane />
         </Button>
       </Group>
     </Box>
