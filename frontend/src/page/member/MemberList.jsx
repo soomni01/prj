53f1ebca784e7e31ffcd0526c3219daceb43b788
soomni01 <dsgh8933@naver.com>
2024-11-22 17:31:24 +0900

    회원 목록 화면 css

--- frontend/src/page/member/MemberList.jsx
+++ frontend/src/page/member/MemberList.jsx
@@ -2,6 +2,7 @@ import { Box, Spinner, Table } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { MyHeading } from "../../components/root/MyHeading.jsx";
 
 export function MemberList() {
   const [memberList, setMemberList] = useState([]);
@@ -12,24 +13,29 @@ export function MemberList() {
     axios.get("/api/member/list").then((res) => setMemberList(res.data));
   }, []);
 
-  if (!memberList || memberList.length === 0) {
-    return <Spinner />;
-  }
-
   // 테이블 행 클릭시 회원정보보기로 이동
   function handleRowClick(id) {
     navigate(`/member/${id}`);
   }
 
+  if (!memberList || memberList.length === 0) {
+    return <Spinner />;
+  }
+
   return (
-    <Box>
-      <h3>회원 목록</h3>
+    <Box
+      mx={"auto"}
+      w={{
+        md: "500px",
+      }}
+    >
+      <MyHeading>회원 목록</MyHeading>
 
       <Table.Root interactive>
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeader>ID</Table.ColumnHeader>
-            <Table.ColumnHeader>이메일</Table.ColumnHeader>
+            <Table.ColumnHeader>EMAIL</Table.ColumnHeader>
             <Table.ColumnHeader>가입일시</Table.ColumnHeader>
           </Table.Row>
         </Table.Header>
