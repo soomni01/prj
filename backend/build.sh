22c4fb285e3a33c3c63e08cf69db02851f00c457
soomni01 <dsgh8933@naver.com>
2024-11-25 11:29:27 +0900

    /error 를 /로 포워드
    frontend, backend 빌드 후 원격서버에 복사하고 docker image 만들어서 실행

---
+++ backend/build.sh
@@ -0,0 +1,13 @@
+rm -rf src/main/resources/static
+
+cd ../frontend
+npm run build
+
+mv dist ../backend/src/main/resources/static
+
+cd ../backend
+
+./gradle bootJar
+
+scp -i src/main/resources/secret/key1121.pem build/libs/backend-0.0.1-SNAPSHOT.jar ubuntu@13.124.228.250:./prj.jar
+ssh -i src/main/resources/secret/key1121.pem ubuntu@13.124.228.250 './run.sh'
